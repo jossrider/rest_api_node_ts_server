6455cfeee486725dde3a295205d31da55b87921d
Joss Rider <jossrider@outlook.com>
2024-06-14 11:00:45 -0400

    Terminamos el backend del proyecto

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 import router from './router'
@@ -22,8 +24,23 @@ connectDB()
 // Instancia de express
 const server = express()
 
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  },
+}
+
+server.use(cors(corsOptions))
+
 // Leer datos de formularios
 server.use(express.json())
+
+server.use(morgan('dev'))
 
 server.use('/api/products', router)
 
